4af40fa6f796aca89f2d1897b76a41e20c580049
maishivamhoo123 <maishivamhoo@gmail.com>
2024-03-09 14:14:38 +0530

    changed about us

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Login from './login'
 import Home from './home'
 import HomePage from './home/home-page'
 import OtpVerify from './auth/otpVerify'
+import AboutUs from './about/about-us';
+// import EVENT1 from './events/eventsection';
 import{BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -23,7 +25,9 @@ function App() {
         <Route path='/home' element={<Home/>}></Route>
         <Route path='/verification' element={<OtpVerify/>}></Route>
         <Route path='/home-page' element={<HomePage/>}></Route>
+        <Route path='/about' element={<AboutUs/>}></Route>
 
+        {/* <Route path='/eventsection' element={<EVENT1/>}></Route> */}
       </Routes>
     
     </BrowserRouter>
